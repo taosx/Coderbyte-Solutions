566b52173e0a1d2d944e927408e93cff1f75007c
taosx <sh4rk1zm4st3rc0der@gmail.com>
2015-10-20 16:43:45 +0300

    First reverse v0.1

---
+++ first_reverse.js
@@ -0,0 +1,13 @@
+function FirstReverse(strp) {
+  var str = '';
+  for (var i = strp.length - 1;i>=0;i--) {
+    str+=strp[i];
+  }
+  // code goes here  
+  return str; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+FirstReverse(readline());           
